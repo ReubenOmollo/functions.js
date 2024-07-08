dd3c3c5ea66eee4455eb30ee6b500339c878549f
ReubenOmollo <reubenochieng23@gmail.com>
2024-07-08 12:37:39 +0300

    hey.js

---
+++ hey.js
@@ -0,0 +1,39 @@
+function calculateBmi( weight,height){
+let bmi = weight / ( height * height);
+return bmi;
+
+}
+function interpretBmi(bmi) 
+if(bmi<18.5) {  
+
+return "underweight";
+
+}else if (bmi<25){   
+
+  return "normal";
+
+}else if(bmi<30) {
+
+    return "overweight";
+ } else { 
+
+    return "obesse";
+ }
+
+
+let weight = 70;
+let height = 1.75;
+
+let bmi = calculateBmi( weight,height);
+console.log("bmi:", bmi );
+console.log( " interpretation", inerpretBmi(bmi))
+
+
+
+
+
+
+
+
+
+
---
+++ scope.js
@@ -0,0 +1,13 @@
+let globalvar = "i'm a global variable";
+
+function demoscope(){
+    let localvar ="i'm a local variable";
+console.log( globalvar);
+console.log( localvar)
+
+}
+
+demoscope();
+
+
+
